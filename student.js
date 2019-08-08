56d8cd3d3fb1a823752e51cd85ec05e0250f71dc
rijil <rijil@qburst.com>
2019-08-08 16:18:07 +0530

    complete upto search. Sorting need to be done.

--- student.js
+++ student.js
@@ -1,13 +1,21 @@
 storage = window.localStorage;
+window.onload = createArrays();
+const student_node = JSON.parse(storage.getItem("student_array"));
+function createArrays(){
+    if(!(storage.getItem("student_array"))){
+        storage.setItem("student_array", "[]");
+        storage.setItem("employee_array", "[]");
+    }
+}
 $.fn.serializeObject = function()
 {
     var object = {};
     var array = this.serializeArray();
     $.each(array, function() {
         if (object[this.name] !== undefined) {
-            if (!object[this.name].push) {
-                object[this.name] = [object[this.name]];
-            }
+            // if (!object[this.name].push) {
+            //     object[this.name] = [object[this.name]];
+            // }
             object[this.name].push(this.value || '');
         } else {
             object[this.name] = this.value || '';
@@ -15,35 +23,72 @@ $.fn.serializeObject = function()
     });
     return object;
 };
-var test = {};
-var i=storage.length;
 $(function() {
     $('.std-form').submit(function() {
-        test = JSON.stringify(($('.std-form').serializeObject()));
-        storage.setItem(i, test);
-        i++;
-        location.reload(); 
-        return false;
-    });
-});
-function loadTableData(){
-    for (var key in localStorage){
-        console.log(key);
-        var obj = storage.getItem(key);
-        var object = JSON.parse(obj);
+        let object = /*JSON.stringify*/($('.std-form').serializeObject());
         let name = object.name;
         let student_id = object.studentid;
         let standard = object.standard;
         let guardian = object.guardian;
         let phone = object.phone;
-        let rows = `<tr><td> ${name}  </td><td>  ${student_id} </td><td> ${standard} </td><td> ${guardian} </td><td>${phone} </td><td><a class="waves-effect waves-light btn" onclick="storage.removeItem(${key});location.reload();"><i class="material-icons">backspace</i></a></td><td><a class="waves-effect waves-light btn modal-trigger" href="#modal1" onclick="ediItem(${key});"><i class="material-icons">edit</i></a></td></tr>`;
+        let rows = `<tr><td> ${name}  </td><td>  ${student_id} </td><td> ${standard} </td><td> ${guardian} </td><td>${phone} </td><td><a class="waves-effect waves-light btn" onclick="removeItem(${student_node.length});"><i class="material-icons">backspace</i></a></td><td><a class="waves-effect waves-light btn modal-trigger" href="#modal1" onclick="editItem(${student_node.length}); "><i class="material-icons">edit</i></a></td></tr>`;
         $(rows).appendTo("#std_tbody");
+        console.log(student_node);
+        student_node.push(object);
+        storage.setItem("student_array",JSON.stringify(student_node));
+        //location.reload(); 
+        return false;
+    });
+});
+function loadTableData(){
+    var list = document.getElementById("std_tbody");
+    var child = list.lastElementChild;  
+        while (child) { 
+            list.removeChild(child); 
+            child = list.lastElementChild; 
+        } 
+    student_node.forEach(element => {
+        let object = element;
+        console.log(object.name);
+        let name = object.name;
+        let student_id = object.studentid;
+        let standard = object.standard;
+        let guardian = object.guardian;
+        let phone = object.phone;
+        let rows = `<tr><td> ${name}  </td><td>  ${student_id} </td><td> ${standard} </td><td> ${guardian} </td><td>${phone} </td><td><a class="waves-effect waves-light btn" onclick="removeItem(${student_node.indexOf(element)})"><i class="material-icons">backspace</i></a></td><td><a class="waves-effect waves-light btn modal-trigger" href="#modal1" onclick="editItem(${student_node.indexOf(element)});"><i class="material-icons">edit</i></a></td></tr>`;
+        $(rows).appendTo("#std_tbody");
+    })
     }
+function removeItem(key){
+    student_node.splice(key,1);
+    storage.setItem("student_array",JSON.stringify(student_node));
+    loadTableData();
 }
-function ediItem(key){
-    let obj = storage.getItem(key);
-    let object = JSON.parse(obj);
-    let i=0;
+function searchItem(value){
+    let filter = value.toUpperCase();
+    table = document.getElementById("std_table");
+    tr = table.getElementsByTagName("tr");
+    var found = true;
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                found = true;
+            }
+        }
+        if (found) {
+            tr[i].style.display = "";
+            found = false;
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+    console.log(test);
+}
+function editItem(key){
+    var i=0;
+    let object = student_node[key];
+    console.log(object);
     for(let property in object ){
         document.forms[0][i].value = object[property];
         i++;
@@ -53,9 +98,10 @@ function ediItem(key){
     const buttonElement = document.getElementById('std_update');
     buttonElement.addEventListener('click', {
         handleEvent: function (event) {
-                test = JSON.stringify(($('.std-form').serializeObject()));
-                storage.setItem(key, test);
-                location.reload();
+                test = (($('.std-form').serializeObject()));
+                student_node[key]=test;
+                storage.setItem("student_array",JSON.stringify(student_node));
+                loadTableData();
             }
-      });
+        });
 }
